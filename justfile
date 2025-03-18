cf966f9712449f94a01468d02e9dbfeb7b0803eb
Matthias (generalsjefsutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2025-03-18 14:55:07 +0100

    abandoned typescript for this project

--- justfile
+++ justfile
@@ -1,9 +1,4 @@
 
-app:
-    tsc -w -p tsconfig.app.json
-
-sw:
-    tsc -w -p tsconfig.sw.json
 
 css:
     sass -w scss/main.scss webapp/app.css
---
+++ webapp/serviceworker.js
@@ -0,0 +1,29 @@
+
+
+console.log('Hello World from service worker');
+
+async function swInit() {
+    const dt= new Date();
+    const hr=dt.getHours();
+    const mn=dt.getMinutes();
+    const sc=dt.getSeconds();
+    console.log('start time:',hr+':'+mn+':'+sc);
+    setTimeout(() => {
+        console.log('Hello World from service worker again');
+        self.registration.showNotification('Hello World', {
+            body: 'Hello World from service worker',
+        });      
+    }, 5_000);
+
+    // self.addEventListener('fetch', (event) => {
+    //     console.log('Fetch event:', event);
+    // });
+    // self.addEventListener('install', (event) => {
+    //     console.log('Install event:', event);
+    // });
+    // self.addEventListener('activate', (event) => {
+    //     console.log('Activate event:', event);
+    // });
+}
+
+swInit().catch(console.error);
