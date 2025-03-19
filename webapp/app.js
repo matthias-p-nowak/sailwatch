737d00b0c32f22dc732770d23d685bfe7e0ac26c
Matthias (generalsjefsutvikler) Nowak <matthiaspeter.nowak@centric.eu>
2025-03-19 13:10:31 +0100

    small things

--- webapp/app.js
+++ webapp/app.js
@@ -1,40 +1,39 @@
 async function main() {
-    let registration=await navigator.serviceWorker.register('serviceworker.js');
-    result=await Notification.requestPermission();
-    console.log('Notification permission:', result);
-    console.log('Service worker registered with scope:', registration.scope);
-    await navigator.serviceWorker.ready;
-    if(registration.active){
-        console.log('Service worker is active');    
-    }else{
-        console.log('Service worker not active');
-    }
-}
+  console.log("Hello World from app");
+  let registration = await navigator.serviceWorker.register("serviceworker.js");
+  result = await Notification.requestPermission();
+  console.log("Notification permission:", result);
+  console.log("Service worker registered with scope:", registration.scope);
+  await navigator.serviceWorker.ready;
+  if (registration.active) {
+    console.log("Service worker is active");
+  } else {
+    console.log("Service worker not active");
+  }
 
-console.log('Hello World from app');
+  const isPwaInstalled =  window.matchMedia("(display-mode: standalone)").matches ||
+    window.navigator.standalone;
+
+  if (isPwaInstalled) {
+    console.log("PWA is installed");
+  } else {
+    console.log("PWA is not installed");
+  }
+}
 main().catch(console.error);
 
-const isPwaInstalled = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone;
-
-if (isPwaInstalled) {
-  console.log('PWA is installed');
-} else {
-  console.log('PWA is not installed');
-}
-
 window.addEventListener("beforeinstallprompt", (event) => {
-    console.log('Before install prompt');
-    let installPrompt = event;
-    const installButton = document.querySelector("#install");
-    installButton.classList.remove("hidden"); 
-    installButton.addEventListener("click", async () => {
-        const result = await installPrompt.prompt();
-        console.log(`Install prompt was: ${result.outcome}`);
-        installPrompt = null;
-        installButton.classList.add("hidden");
-        console.log('Install button clicked');
-    });
-    
+  console.log("Before install prompt");
+  let installPrompt = event;
+  const installButton = document.querySelector("#install");
+  installButton.classList.remove("hidden");
+  installButton.addEventListener("click", async () => {
+    const result = await installPrompt.prompt();
+    console.log(`Install prompt was: ${result.outcome}`);
+    installPrompt = null;
+    installButton.classList.add("hidden");
+    console.log("Install button clicked");
+  });
 });
 
-document.getElementById('commit-hash').innerHTML=gitversion;
+document.getElementById("commit-hash").innerHTML = gitversion;
--- webapp/serviceworker.js
+++ webapp/serviceworker.js
@@ -1,54 +1,55 @@
 // Note: This is the service worker file that will be registered in the browser
-console.log('Hello World from service worker 1');
+console.log("Hello World from service worker 1");
 
 async function swInit() {
-    const dt= new Date();
-    const hr=dt.getHours();
-    const mn=dt.getMinutes();
-    const sc=dt.getSeconds();
-    console.log('start time:',hr+':'+mn+':'+sc);
-    
-    setTimeout(() => { 
-        console.log('Notification');
-        self.registration.showNotification('Sailwatch', {
-            body: 'goto window',
-            icon: 'icon.png',
-            badge: 'badge.png',
-            vibrate: [100, 50, 100],
-            requireInteraction: true,
+  const dt = new Date();
+  const hr = dt.getHours();
+  const mn = dt.getMinutes();
+  const sc = dt.getSeconds();
+  console.log("start time:", hr + ":" + mn + ":" + sc);
 
-        });      
-    }, 10_000);
-
-    self.addEventListener('notificationclick', async (event) => {
-        console.log('Notification click:', event);
-        event.notification.close();
-       
-  event.waitUntil(
-    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
-      for (const client of clientList) {
-        if ('focus' in client) {
-            console.log('focusing');
-          return client.focus();
-        }
-      }
-      if (self.clients.openWindow) {
-        console.log('opening window');
-        return self.clients.openWindow('.');
-      }
-    })
-  );
-      });
-
-    // self.addEventListener('fetch', (event) => {
-    //     console.log('Fetch event:', event);
-    // });
-    // self.addEventListener('install', (event) => {
-    //     console.log('Install event:', event);
-    // });
-    // self.addEventListener('activate', (event) => {
-    //     console.log('Activate event:', event);
-    // });
+  setTimeout(() => {
+    console.log("Notification");
+    self.registration.showNotification("Sailwatch", {
+      body: "goto window",
+      icon: "icon.png",
+      badge: "badge.png",
+      vibrate: [100, 50, 100],
+      requireInteraction: true,
+    });
+  }, 10_000);
 }
+
+self.addEventListener("notificationclick", async (event) => {
+  console.log("Notification click:", event);
+  event.notification.close();
+
+  event.waitUntil(
+    self.clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if ("focus" in client) {
+            console.log("focusing");
+            return client.focus();
+          }
+        }
+        if (self.clients.openWindow) {
+          console.log("opening window");
+          return self.clients.openWindow(".");
+        }
+      })
+  );
+});
+
+// self.addEventListener('fetch', (event) => {
+//     console.log('Fetch event:', event);
+// });
+// self.addEventListener('install', (event) => {
+//     console.log('Install event:', event);
+// });
+// self.addEventListener('activate', (event) => {
+//     console.log('Activate event:', event);
+// });
 
 swInit().catch(console.error);
