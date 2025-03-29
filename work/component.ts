3e21930ad8bac12bcb2303bd89062e2dec1852e0
Matthias <me-github@mefju7.priv.no>
2025-03-29 11:35:03 +0100

    it's working

--- work/component.ts
+++ work/component.ts
@@ -25,8 +25,22 @@ export class WebComponent {
             return null;
         }
         const ret = new this();
-        ret.element = template.content.cloneNode(true) as HTMLElement;
-        
+        let clonedNode = template.content.cloneNode(true) as DocumentFragment;
+        ret.element  = clonedNode.firstElementChild as HTMLElement;
+        WebComponent.fillElement<T>(ret);
+        return ret;
+    }
+
+    static fromElement<T extends WebComponent>(this: new()=> T, elem: HTMLElement): T|null {
+        if(elem == null)
+            return null;
+        const ret = new this();
+        ret.element = elem;
+        WebComponent.fillElement<T>(ret);
+        return ret;
+    }
+
+    private static fillElement<T extends WebComponent>(ret: T) {
         Object.entries(ret).forEach(([key, value], idx) => {
             console.log(`got ${idx}: key=${key} value=${value}`);
             if (value == undefined) {
@@ -36,16 +50,14 @@ export class WebComponent {
                 console.log(`what is ${key}?`, value);
             }
         });
-        WebComponent.getEventFunctions(ret).forEach((value, idx)=>{
-            let fn=value as string;
+        WebComponent.getEventFunctions(ret).forEach((value, idx) => {
+            let fn = value as string;
             console.log(`assigning ${idx} ${fn}`);
-            let parts=fn.split('_on');
-            let eventFn='on'+parts[1];
-            ret[parts[0]][eventFn]=ret[fn].bind(ret);
-        })
-        return ret;
+            let parts = fn.split('_on');
+            let eventFn = 'on' + parts[1];
+            ret[parts[0]][eventFn] = ret[fn].bind(ret);
+        });
     }
-    
 }
 
 export class Start extends WebComponent {
@@ -53,6 +65,7 @@ export class Start extends WebComponent {
     pressme: HTMLButtonElement;
     pressme_onclick(event){
         console.log('got a click', event,this);
+        this.pressme.innerText='okok';
     }
 }
 
@@ -60,6 +73,11 @@ console.log('making x');
 let x = Start.fromTemplate();
 if (x) {
     console.log('got ', x);
+    x.element.id='yellow';
     document.getElementById('main')?.append(x.element);
     x.notice.innerText='new notice';
+    console.log(`x.element is ${x.element.id}`);
 }
+
+let y=Start.fromElement(document.getElementById('yellow')!);
+y!.notice.innerText='submarine';
