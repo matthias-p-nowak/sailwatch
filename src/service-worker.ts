89d46cdfa4b73e9d81cde9561016f4f46de8ad08
Matthias <me-github@mefju7.priv.no>
2025-04-20 20:10:23 +0200

    service worker with event handlers

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,1 +1,30 @@
-console.log('service worker is starting');
+/// <reference lib="webworker" />
+
+
+let gitVersion='currentGitVersion';
+console.log(`running inside service worker gitVersion=${gitVersion}`);
+
+let sw=self as unknown as ServiceWorkerGlobalScope;
+
+
+sw.oninstall = function (event) {
+    console.log("service worker is installed");
+    // event.waitUntil();
+};
+sw.onactivate = function (event) {
+    console.log("service worker is activated");
+    // event.waitUntil();
+};
+sw.onoffline = function (event) {
+    console.log("service worker is offline");
+    // event.waitUntil();
+};
+sw.ononline= function (event) {
+    console.log("service worker is online");
+    // event.waitUntil();
+};
+
+sw.onfetch = function (event) {
+    console.log("service worker is fetching");
+    event.respondWith(fetch(event.request));
+};
